ab645fbec2ce7643375f4a908023ed499dddea7a
Caio Carvalho <caio.cmalves@gmail.com>
2022-08-26 16:00:01 -0300

    resolução ex 26 e 27

--- ex26.py
+++ ex26.py
@@ -1,5 +1,5 @@
 nome = input("Digite uma frase: ").strip().lower()
 
-print("A letra A aparece {} vezes na frase.".format())
-print("A primeira letra A apareceu na posição {}")
-print("A última letra A apareceu na posição {}")
+print("A letra A aparece {} vezes na frase.".format(nome.count("a")))
+print("A primeira letra A apareceu na posição {}".format(nome.find("a") + 1))
+print("A última letra A apareceu na posição {}".format(nome.rfind("a") + 1))
---
+++ ex27.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome completo: ").strip()
+
+print("Muito prazer em te conhecer!")
+print("Seu primero nome é {}".format(nome.split(" ")[0]))
+print("Seu último nome é {}".format(nome.split(" ")[-1]))
