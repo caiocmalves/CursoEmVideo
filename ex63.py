f0e16e73770b49759156ce84df54e4eac9775a70
Caio Carvalho <caio.cmalves@gmail.com>
2022-09-06 21:14:51 -0300

    resolução exercício 63

--- ex63.py
+++ ex63.py
@@ -1,3 +1,11 @@
 print('-' * 20)
 print('Sequêbcia de Fibonacci')
 print('-' * 20)
+termos = int(input('Quantos termos você quer mostrar? '))
+fibo = [0, 1, 1]
+for i in range(3,termos):
+    fibo.append(fibo[i - 1] + fibo[i - 2]) 
+
+for i in fibo:
+    print(i, end=' -> ')
+print('FIM')
